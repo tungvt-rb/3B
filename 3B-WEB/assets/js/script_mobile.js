b9e064b237e7d10963c640946a4195814bc4d5bf
TungVT <tungvt1611@gmail.com>
2021-08-21 21:23:15 +0700

    3B - NEW_DESIGN - SEARCH_NOT_FOUND_MOBILE - add search-not-found page

--- 3B-WEB/assets/js/script_mobile.js
+++ 3B-WEB/assets/js/script_mobile.js
@@ -1,7 +1,10 @@
-(function($){
-	$(function(){
-		$(".slb").select2({
-			width: null
-		});
+(function($) {
+	$(function() {
+		var slb = $('.slb');
+		if (slb.length) {
+			$(".slb").select2({
+				width: null
+			});
+		}
 	});
 })(jQuery);
