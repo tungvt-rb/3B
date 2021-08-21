8f7b1a7a0c6ea157fe099f8f8eb489f77f08a3e4
TungVT <tungvt1611@gmail.com>
2021-08-21 23:02:02 +0700

    3B - NEW_DESIGN - SEARCH_MOBILE - add popup search on header top

--- 3B-WEB/assets/js/script_mobile.js
+++ 3B-WEB/assets/js/script_mobile.js
@@ -1,5 +1,20 @@
 (function($) {
 	$(function() {
+		$('.search-control .form-control').focus(function(){
+			$('.advance-search').addClass('active');
+		});
+		$('.search-by .close').click(function() {
+			$('.advance-search').removeClass('active');
+		});
+		$('.by-store').click(function() {
+			$(this).addClass('active');
+			$('.by-product').removeClass('active');
+		});
+		$('.by-product').click(function() {
+			$(this).addClass('active');
+			$('.by-store').removeClass('active');
+		});
+
 		var slb = $('.slb');
 		if (slb.length) {
 			$(".slb").select2({
