200084796f7839ea3f4e835d87d9d72c2ea661d3
TungVT <tungvt1611@gmail.com>
2021-08-25 11:17:43 +0700

    3B - NEW_DESIGN - HOME_MOBILE - add search result list store and product

--- 3B-WEB/assets/js/script_mobile.js
+++ 3B-WEB/assets/js/script_mobile.js
@@ -3,16 +3,25 @@
 		$('.search-control .form-control').focus(function(){
 			$('.advance-search').addClass('active');
 		});
-		$('.search-by .close').click(function() {
-			$('.advance-search').removeClass('active');
+		$('.advance-search .close').click(function() {
+			$('.search-history').show();
+			$('.advance-search, .by-store, .by-product, .results').removeClass('active');
 		});
 		$('.by-store').click(function() {
 			$(this).addClass('active');
 			$('.by-product').removeClass('active');
+
+			$('.search-history').hide();
+			$('.search-store').addClass('active');
+			$('.search-product').removeClass('active');
 		});
 		$('.by-product').click(function() {
 			$(this).addClass('active');
 			$('.by-store').removeClass('active');
+
+			$('.search-history').hide();
+			$('.search-store').removeClass('active');
+			$('.search-product').addClass('active');
 		});
 
 		var slb = $('.slb');
